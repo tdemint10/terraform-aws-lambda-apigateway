8055b2997d0dceb13a05378f932c24331efc191c
Travis DeMint <deminttravis9@gmail.com>
2024-04-19 08:43:13 -0500

    Extend example

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -2,7 +2,20 @@ terraform {
   required_version = "~> 1.8"
 }
 
+# trivy:ignore:AVD-AWS-0190: Caching not needed for example
 module "apigateway" {
   source = "../.."
-  name   = "example"
+
+  domain_certificate_arn = "arn::MyServiceDomain"
+  domain_name            = "MyServiceDomain"
+  service                = "MyService"
+
+  endpoints = {
+    "GET /v1/greeting" : {
+      lambda = {
+        name       = "Greeting"
+        invoke_arn = "arn::GreetingLambda"
+      }
+    }
+  }
 }
--- examples/simple/outputs.tf
+++ examples/simple/outputs.tf
@@ -1,4 +1,24 @@
-output "greeting" {
-  value       = module.apigateway.greeting.value
-  description = "A standard greeting"
+output "apigateway_domain_name" {
+  value       = module.apigateway.apigateway_domain_name
+  description = "Regional domain name of the APIGateway, for use with custom DNS records"
+}
+
+output "apigateway_domain_zone" {
+  value       = module.apigateway.apigateway_domain_zone
+  description = "Regional hosted zone ID of the APIGateway's domain, for use with custom DNS records"
+}
+
+output "openapi_spec_json" {
+  value       = module.apigateway.openapi_spec_json
+  description = "The OpenAPI specification (in JSON) used to configure the APIGateway"
+}
+
+output "openapi_spec_yaml" {
+  value       = module.apigateway.openapi_spec_yaml
+  description = "The OpenAPI specification (in YAML) used to configure the APIGateway"
+}
+
+output "stage_arn" {
+  value       = module.apigateway.stage_arn
+  description = "ARN of the APIGateway stage"
 }
