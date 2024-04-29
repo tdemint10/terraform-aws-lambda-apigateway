65d72339966f68d1d2bb696ec2d8b955692f2d99
Travis DeMint <deminttravis9@gmail.com>
2024-04-29 13:32:38 -0500

    Update example

--- examples/simple/outputs.tf
+++ examples/simple/outputs.tf
@@ -13,11 +13,6 @@ output "openapi_spec_json" {
   description = "The OpenAPI specification (in JSON) used to configure the APIGateway"
 }
 
-output "openapi_spec_yaml" {
-  value       = module.apigateway.openapi_spec_yaml
-  description = "The OpenAPI specification (in YAML) used to configure the APIGateway"
-}
-
 output "stage_arn" {
   value       = module.apigateway.stage_arn
   description = "ARN of the APIGateway stage"
