27ec5e1cb2cdd28a57cd5f4f7c07da2f780b60aa
Travis DeMint <deminttravis9@gmail.com>
2024-05-09 14:04:37 -0500

    Use policy document rather than JSON policies

--- iam.tf
+++ iam.tf
@@ -35,44 +35,44 @@ data "aws_iam_policy_document" "apigateway_authorizers" {
 }
 
 resource "aws_iam_role" "api_gateway_account_role" {
-  name = "api-gateway-account-role"
-  assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Sid" : "",
-        "Effect" : "Allow",
-        "Principal" : {
-          "Service" : "apigateway.amazonaws.com"
-        },
-        "Action" : "sts:AssumeRole"
-      }
-    ]
-  })
+  name               = "api-gateway-account-role"
+  assume_role_policy = data.aws_iam_policy_document.api_gateway_account_role_trust.json
+}
+
+data "aws_iam_policy_document" "api_gateway_account_role_trust" {
+  version = "2012-10-17"
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["apigateway.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role_policy" "api_gateway_cloudwatch_policy" {
+  name   = "api-gateway-cloudwatch-policy"
+  role   = aws_iam_role.api_gateway_account_role.id
+  policy = data.aws_iam_policy_document.api_gateway_cloudwatch_policy.json
 }
 
 # trivy:ignore:AVD-AWS-0057
-resource "aws_iam_role_policy" "api_gateway_cloudwatch_policy" {
-  name = "api-gateway-cloudwatch-policy"
-  role = aws_iam_role.api_gateway_account_role.id
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:DescribeLogGroups",
-          "logs:DescribeLogStreams",
-          "logs:PutLogEvents",
-          "logs:GetLogEvents",
-          "logs:FilterLogEvents"
-        ],
-        "Resource" : "*"
-      }
+data "aws_iam_policy_document" "api_gateway_cloudwatch_policy" {
+  version = "2012-10-17"
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams",
+      "logs:PutLogEvents",
+      "logs:GetLogEvents",
+      "logs:FilterLogEvents"
     ]
-  })
+    resources = ["*"]
+  }
 }
 
 resource "aws_api_gateway_account" "api_gateway_account" {
